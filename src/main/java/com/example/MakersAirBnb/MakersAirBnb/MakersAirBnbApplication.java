b90060eedce8a9e158e3e5819330839b0ad68553
“JamesRotimi” <jamesrotimi@icloud.com>
2019-03-18 12:02:21 +0000

    Setup

---
+++ src/main/java/com/example/MakersAirBnb/MakersAirBnb/MakersAirBnbApplication.java
@@ -0,0 +1,13 @@
+package com.example.MakersAirBnb.MakersAirBnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakersAirBnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakersAirBnbApplication.class, args);
+	}
+
+}
